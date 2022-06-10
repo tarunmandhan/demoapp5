3ee8c43a88f19af47b419bd1af977cc065ccdb5d
tarunmandhan <mandhantarun@gmail.com>
2022-06-10 18:39:39 +0530

    uploading

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let curDate = new Date();
+curDate = curDate.getHours();
+let greeting ="";
+const cssStyle = {};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+if(curDate>=3 && curDate<12){
+  greeting="Good Morning";
+  cssStyle.color = "green" ;
+} else if(curDate>=12 && curDate<15){
+  greeting="Good Afternoon";
+  cssStyle.color = '#104b79' ;
+} else if(curDate>=15 && curDate<20){
+  greeting="Good Evening";
+  cssStyle.color = '#ff5722' ;
+} else{
+  greeting="Good Night";
+  cssStyle.color = "#604439" ;
+}
+
+ReactDOM.render(
+  <>
+  <div>
+  <h1> 
+    Hello Tarun : <span style={cssStyle} > {greeting} </span> 
+  </h1>
+  </div>
+  </>
+  ,document.getElementById("root"));
